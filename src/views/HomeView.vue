d878ab4c12570468bff8c9ff3c314a155ae87717
Yakup Yaşa <yakupyasa1@gmail.com>
2022-12-02 20:26:50 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -82,7 +82,7 @@ async function getURLs() {
             d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <span>{{ errorAlert.value }}</span>
+        <span>{{ errorAlert }}</span>
       </div>
       <div class="flex-none">
         <button class="btn btn-sm btn-ghost" @click="errorAlert = null">
