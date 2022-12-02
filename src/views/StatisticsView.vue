29fb16d72bb5a1ed6d5f453cb5a62df909f6206f
Yakup Yaşa <yakupyasa1@gmail.com>
2022-12-02 07:46:19 +0300

    Update StatisticsView.vue

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -62,7 +62,7 @@
             </th>
             <th>
               <a
-                target="_blank"
+                target="_self"
                 :href="link.shortenedUrl"
                 class="link link-primary"
                 >{{ link.shortenedUrl }}</a
